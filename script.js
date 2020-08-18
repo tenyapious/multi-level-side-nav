178196255e035640fcdff9f2110855ababe7ed49
Dretekin <dretekin@gmail.com>
2020-08-18 08:54:39 -0400

    refactor

--- script.js
+++ script.js
@@ -1,67 +1,42 @@
 const slider = document.querySelector(".slider");
-const parents = document.querySelector(".parents");
-const children = document.querySelector(".children");
-const grandChildren = document.querySelector(".grand-children");
+// const parents = document.querySelector(".parents");
+// const children = document.querySelector(".children");
+// const grandChildren = document.querySelector(".grand-children");
 const openNav = document.querySelector(".open-nav");
 const openChild = document.querySelector(".open-child");
 const openGrandChild = document.querySelector(".open-grand-child");
-const goBacks = document.querySelectorAll(".go-back");
-let hideChildren = false;
-let descendant = "";
 
-let tl = gsap.timeline({ defaults: { duration: 1 } });
+let tl = gsap.timeline({ defaults: { duration: 0.5, ease: "power4.out" } });
 
 openNav.addEventListener("click", function () {
-  tl.set(slider, { display: "block" }).to(slider, { x: "100%" });
+  tl.set(slider, { display: "block" }).to(slider, { xPercent: 100 });
 });
 
-function transitioner(show, hide) {}
-
-openChild.addEventListener("click", function () {
+function transitioner(show, hide, direction = "+=100") {
   if (!tl.isActive()) {
-    descendant = parents;
-    tl.set(children, { display: "block" }).to([parents, children], {
-      x: "100%",
+    tl.set(show, { display: "block" }).to([show, hide], {
+      xPercent: direction,
       onComplete: function () {
-        parents.style.display = "none";
+        hide.style.display = "none";
       },
     });
   }
+}
+
+openChild.addEventListener("click", function () {
+  transitioner(this.parentElement.nextElementSibling, this.parentElement);
 });
 
 openGrandChild.addEventListener("click", function () {
-  if (!tl.isActive()) {
-    descendant = children;
-    hideChildren = true;
-    tl.set(grandChildren, { display: "block" }).to([children, grandChildren], {
-      x: "+=100%",
-      onComplete: function () {
-        children.style.display = "none";
-      },
-    });
-  }
+  transitioner(this.parentElement.nextElementSibling, this.parentElement);
 });
 
-goBacks.forEach(function (goBack) {
+gsap.utils.toArray(".go-back").forEach((goBack) => {
   goBack.addEventListener("click", function () {
-    if (!tl.isActive()) {
-      descendant.style.display = "block";
-      if (descendant == parents) {
-        tl.to([this.parentElement, descendant], {
-          x: 0,
-          onComplete: () => {
-            this.parentElement.style.display = "none";
-          },
-        });
-      } else if (descendant == children) {
-        tl.to([this.parentElement, descendant], {
-          x: "-=100%",
-          onComplete: () => {
-            this.parentElement.style.display = "none";
-            descendant = parents;
-          },
-        });
-      }
-    }
+    transitioner(
+      this.parentElement.previousElementSibling,
+      this.parentElement,
+      "-=100"
+    );
   });
 });
