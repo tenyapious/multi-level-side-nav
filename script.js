160650c4a9d8dcba62a7e62278d949391368b926
Dretekin <dretekin@gmail.com>
2020-08-17 22:35:32 -0400

    code cleanup

--- script.js
+++ script.js
@@ -9,87 +9,59 @@ const goBacks = document.querySelectorAll(".go-back");
 let hideChildren = false;
 let descendant = "";
 
+let tl = gsap.timeline({ defaults: { duration: 1 } });
+
 openNav.addEventListener("click", function () {
-  slider.style.display = "block";
-  gsap.to(slider, {
-    duration: 1,
-    x: "100%",
-  });
+  tl.set(slider, { display: "block" }).to(slider, { x: "100%" });
 });
 
-function transitioner(show, hide) {
-  show.style.display = "block";
-  hide.style.transform = "translateX(100%)";
-  setTimeout(function () {
-    show.style.transform = "translateX(100%)";
-  });
-}
+function transitioner(show, hide) {}
 
 openChild.addEventListener("click", function () {
-  descendant = parents;
-  children.style.display = "block";
-  gsap.to([parents, children], {
-    duration: 1,
-    x: "100%",
-    onComplete: function () {
-      parents.style.display = "none";
-    },
-  });
+  if (!tl.isActive()) {
+    descendant = parents;
+    tl.set(children, { display: "block" }).to([parents, children], {
+      x: "100%",
+      onComplete: function () {
+        parents.style.display = "none";
+      },
+    });
+  }
 });
 
 openGrandChild.addEventListener("click", function () {
-  descendant = children;
-  hideChildren = true;
-  grandChildren.style.display = "block";
-
-  // gsap.to(grandChildren, {
-  //   duration: 1,
-  //   x: "100%",
-  // });
-
-  gsap.to([children, grandChildren], {
-    duration: 1,
-    x: "+=100%",
-    onComplete: function () {
-      children.style.display = "none";
-    },
-  });
+  if (!tl.isActive()) {
+    descendant = children;
+    hideChildren = true;
+    tl.set(grandChildren, { display: "block" }).to([children, grandChildren], {
+      x: "+=100%",
+      onComplete: function () {
+        children.style.display = "none";
+      },
+    });
+  }
 });
 
 goBacks.forEach(function (goBack) {
   goBack.addEventListener("click", function () {
-    descendant.style.display = "block";
-    // this.parentElement.style.transform = "translate(0)";
-    // setTimeout(function () {
-    //   if (descendant == parents) {
-    //     descendant.style.transform = "translateX(0)";
-    //   } else if (descendant == children) {
-    //     descendant.style.transform = "translateX(100%)";
-    //   }
-    // });
-    if (descendant == parents) {
-      gsap.to([this.parentElement, descendant], {
-        duration: 1,
-        x: 0,
-        onComplete: () => {
-          this.parentElement.style.display = "none";
-        },
-      });
-    } else if (descendant == children) {
-      // descendant.style.transform = "translateX(100%)";
-      gsap.to(this.parentElement, {
-        duration: 1,
-        x: "0%",
-        onComplete: () => {
-          this.parentElement.style.display = "none";
-          descendant = parents;
-        },
-      });
-
-      gsap.to(descendant, {
-        duration: 1,
-        x: "100%",
-      });
+    if (!tl.isActive()) {
+      descendant.style.display = "block";
+      if (descendant == parents) {
+        tl.to([this.parentElement, descendant], {
+          x: 0,
+          onComplete: () => {
+            this.parentElement.style.display = "none";
+          },
+        });
+      } else if (descendant == children) {
+        tl.to([this.parentElement, descendant], {
+          x: "-=100%",
+          onComplete: () => {
+            this.parentElement.style.display = "none";
+            descendant = parents;
+          },
+        });
+      }
     }
   });
 });
