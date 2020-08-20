be215bd2240cb33aacd7d9ec17c1d62d7eb87931
Dretekin <dretekin@gmail.com>
2020-08-20 11:40:01 -0400

    add debounce from lodash and fix resize issues

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const container = document.querySelector(".container");
 const slider = document.querySelector(".nav");
 const parentsContainer = document.querySelector(".parents");
 const childrenContainer = document.querySelector(".children");
@@ -9,14 +10,34 @@ const containerDarkCover = document.querySelector(".container-dark-cover");
 
 let childrensContainersChildToShow = "",
   grandchildrensContainersChildToShow = "",
-  childrenContainerBool = false;
-
-let viewPortHeight =
-  window.innerHeight ||
-  document.documentElement.clientHeight ||
-  document.body.clientHeight;
+  childrenContainerBool = false,
+  viewPortHeight = window.innerHeight;
+navOpenedBool = false;
 
 document.documentElement.style.setProperty("--vh", `${viewPortHeight}px`);
+
+// /////////////////////////////////
+
+function reportWindowSize() {
+  viewPortHeight = window.innerHeight;
+  document.documentElement.style.setProperty("--vh", `${viewPortHeight}px`);
+  if (navOpenedBool) container.style.height = viewPortHeight + "px";
+  else {
+    container.style.height = "100%";
+  }
+  // container.style.height = viewPortHeight;
+  // console.log(viewPortHeight);
+}
+
+// jQuery(window).on("resize", _.debounce(reportWindowSize, 150));
+// jQuery(window).on("resize", reportWindowSize);
+
+window.onresize = _.debounce(reportWindowSize, 200, {
+  leading: false,
+  trailing: true,
+});
+
+// /////////////////////////////////
 
 let tl = gsap.timeline({
   defaults: { duration: 0.5, ease: "power1.power1.inOut" },
@@ -25,7 +46,7 @@ let tl = gsap.timeline({
 let openNavTl = gsap
   .timeline({ defaults: { duration: 0.5, ease: "power1.inOut" } })
   .set([slider, containerDarkCover], { display: "block" })
-  .set(".container", { height: viewPortHeight })
+  // .set(container, { height: viewPortHeight })
   .to(slider, { xPercent: 100 })
   .to(
     ".container-cover",
@@ -35,13 +56,20 @@ let openNavTl = gsap
     0
   );
 
+// const containerReSize = gsap.quickSetter(container, "height", "px");
+
 openNavTl.pause();
 
 openNav.addEventListener("click", function () {
+  navOpenedBool = true;
+  container.style.height = viewPortHeight + "px";
+  // containerReSize(viewPortHeight);
   openNavTl.restart();
 });
 
 containerDarkCover.addEventListener("click", function () {
+  navOpenedBool = false;
+  container.style.height = "100%";
   openNavTl.reverse();
 });
 
